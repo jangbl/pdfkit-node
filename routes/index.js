2bb8ddc0610c8cb8343d6feaeba3a38af73da018
productioncoder <productioncoding@gmail.com>
2021-05-10 12:13:28 +0200

    correct typo

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/invoice', (req, res, next) => {
   const stream = res.writeHead(200, {
     'Content-Type': 'application/pdf',
-    'Content-disposition': `attachment;filename=invoice.pdf`,
+    'Content-Disposition': `attachment;filename=invoice.pdf`,
   });
   pdfService.buildPDF(
     (chunk) => stream.write(chunk),
